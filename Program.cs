35e221ec3e7b87e97d0f2fcac3d9f4fc0a2ba197
mohanreddynandaluru <mohanreddy232312@gmail.com>
2026-03-09 21:20:27 +0530

    BMI calculator

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace BMI_index
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            float height, weight;
+            Console.WriteLine("Enter Height (CM)");
+            while (true)
+            {
+                if (!float.TryParse(Console.ReadLine(), out height))
+                {
+                    Console.WriteLine("Enter correct Height");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Enter weight");
+            while (true)
+            {
+                if (!float.TryParse(Console.ReadLine(), out weight))
+                {
+                    Console.WriteLine("Enter correct Weight");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            BMI bmi = new BMI(height, weight);
+
+            bmi.GetBMI();
+        }
+    }
+}
