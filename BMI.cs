4e64aaad6dce98b8718b42624e9ee1fecad471c9
mohanreddynandaluru <mohanreddy232312@gmail.com>
2026-03-17 22:15:50 +0530

    POC for Constructor

---
+++ BMI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace BMI_index
+{
+    internal class BMI
+    {
+        float height;
+        float weight;
+        float bmi;
+        // constructor
+        public BMI(float height)
+        {
+            this.height = (height/100);
+        }
+        // Constructor OverLoading and Constructor Chaining.
+        public BMI(float height, float weight) : this(height)
+        {
+            this.weight = weight;
+        }
+
+        public void GetBMI()
+        {
+            bmi = weight / (height * height);
+
+            // Boxing
+            object boxedBMI = bmi;
+
+            Console.WriteLine("Boxed BMI value: " + boxedBMI);
+
+            // Unboxing
+            float unboxedBMI = (float)boxedBMI;
+
+            Console.WriteLine("Unboxed BMI value: " + unboxedBMI);
+        }
+
+    }
+}
